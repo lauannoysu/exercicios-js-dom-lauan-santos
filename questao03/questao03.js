871e56faf5b49dd5089981d6727ccefb86f59c72
lauannoysu <lauangsantos@gmail.com>
2025-12-03 17:56:41 -0300

    Add files via upload

---
+++ questao03/questao03.js
@@ -0,0 +1,13 @@
+function cadastrarUsuario()
+{
+    let nome = document.getElementById("nome").value;
+    let dataNasc = document.getElementById("dataNasc").value;
+    let rua = document.getElementById("rua").value;
+    let numero = document.getElementById("numero").value;
+    let cidade = document.getElementById("cidade").value;
+    let uf = document.getElementById("uf").value;
+
+    let frase = `${nome} nasceu no dia ${dataNasc} e mora na rua ${rua}, número ${numero}, na cidade ${cidade} do estado ${uf}.`
+
+    document.getElementById("resultado").innerText = frase;
+}
